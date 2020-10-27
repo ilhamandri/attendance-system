edb23ce995f663b26e0b14e95dfce138a1c48faa
ilhamandri <ilhamandriandri@gmail.com>
2020-10-27 19:13:30 +0700

    permit user & address for devise registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,35 @@ class UsersController < ApplicationController
   def index
     # @user = User.find(params[:id])
   end
+
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :address, :email)
+    end
+
 end
